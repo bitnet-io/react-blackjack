0bb17746f975200bd98030eb101b5de59bc71be7
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-01-14 06:20:09 -0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ import ReactDOM  from 'react-dom';
 
 import App from './components/app';
 // import game classes
-import Hand from './game/Hand';
-import Deck from './game/Deck';
+import Hand from './game/hand.js';
+import Deck from './game/deck.js';
 // import game logic
 import { getWinner, dealerDrawing } from './game';
 
